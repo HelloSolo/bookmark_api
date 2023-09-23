e6b4ddd49f10d3df5ff92a24a7c18979f136bc4b
davidalimi148@gmail.com <davidalimi148@gmail.com>
2023-09-23 18:58:59 +0100

    installed flask_jwt_extender

--- src/__init__.py
+++ src/__init__.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask
+from flask_jwt_extended import JWTManager
 from src.auth import auth
 from src.bookmarks import bookmarks
 from src.database import db
@@ -12,6 +13,7 @@ def create_app(test_config=None):
         app.config.from_mapping(
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
+            JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
         )
     else:
         app.config.from_mapping(test_config)
@@ -26,6 +28,8 @@ def create_app(test_config=None):
 
     db.app = app
     db.init_app(app)
+
+    JWTManager(app)
     app.register_blueprint(auth)
     app.register_blueprint(bookmarks)
 
