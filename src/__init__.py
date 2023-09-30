a463548adaff5b342cacd10b7775d012ddcfad8a
davidalimi148@gmail.com <davidalimi148@gmail.com>
2023-09-30 15:46:51 +0100

    increased jwt token validity period

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, redirect, jsonify
 from flask_jwt_extended import JWTManager
 from flasgger import Swagger, swag_from
+from datetime import timedelta
 from src.config.swagger import template, swagger_config
 from src.auth import auth
 from src.bookmarks import bookmarks
@@ -17,6 +18,7 @@ def create_app(test_config=None):
             SECRET_KEY=os.environ.get("SECRET_KEY"),
             SQLALCHEMY_DATABASE_URI=os.environ.get("SQLALCHEMY_DATABASE_URI"),
             JWT_SECRET_KEY=os.environ.get("JWT_SECRET_KEY"),
+            JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=1),
             SWAGGER={"title": "Bookmark API", "uiversion": 3},
         )
     else:
